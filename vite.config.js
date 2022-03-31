55df67a6bc1c56285c6933b0fca4a518d3b59775
mirzaromi <mirzaromi12@gmail.com>
2022-03-31 11:37:47 +0700

    change commit

--- vite.config.js
+++ vite.config.js
@@ -11,5 +11,6 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: '/vue-reaction-time'
+  publicPath: process.env.NODE_ENV === 'production' ? '/vue-reaction-time/' : '/'
+  // base: '/'
 })
